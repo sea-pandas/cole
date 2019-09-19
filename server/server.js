745918e4650e96953dd772742ab76e68f2af7efa
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2019-09-19 13:39:24 -0700

    authentication working

--- server/server.js
+++ server/server.js
@@ -1,40 +1,28 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-const PORT = 6666;
-const path = require('path');
-
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
-app.use(bodyparser.json());
-app.use(cookieParser());
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT, () => console.log('server listening on Port:', PORT));
-=======
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const messageModel = require('./models/requestModel');
+const userController = require('./user/userController');
 
-
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 //will give req.cookies
-// app.use(cookieParser());
+app.use(cookieParser());
+
+app.post('/login', userController.verify, (req, res) => {
+  res.send('sucessfully logged in');
+})
+
+app.post('/signup', userController.signup, (req, res) => {
+  res.send('successfully signed up')
+})
+
+
 
 // app.use(express.static('client'));
 
 
 app.listen(3434, () => console.log('listening on Port 3434'));
->>>>>>> master
