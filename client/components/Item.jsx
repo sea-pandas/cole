da9bd9860993bec2435429b51611db118a20b808
Alexander <alxdip123@gmail.com>
2019-09-19 16:29:20 -0700

    last minute client additions

--- client/components/Item.jsx
+++ client/components/Item.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default (props) => {
   return <li>
-    {props.name}
+    item: {props.name} <br />
+    description: {props.description} <br />
+    user: {props.username} <br />
+    votes: {props.vote} <br />
   </li>
 }
--- client/components/Items.jsx
+++ client/components/Items.jsx
@@ -4,7 +4,7 @@ import Item from './Item.jsx'
 export default (props) => {
   return <div>
     <ul>
-      {props.items.map((el, idx) => <Item key={idx} name={el.name}/>)}
+      {props.items.map((el, idx) => <Item key={idx} name={el.name} description={el.description} vote={el.vote} username={el.username} />)}
     </ul>
   </div>
 }
